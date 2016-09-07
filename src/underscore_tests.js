32d96d7a93c2a18b92a64310b90481edb1505744
Talon Hughes <talon.hughes@gmail.com>
2016-09-06 21:04:06 -0600

    my answers so far

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,21 +16,49 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if(isNaN(n)){
+      return array[0];
+    }
+
+    if(n >= array.length){
+      return array;
+    }
+    var newArray = [];
+    for (var i = 0; i < n; i++){
+      newArray.push(array[i]);
+    }
+    return newArray;
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(isNaN(n)){
+      return array[array.length - 1];
+    }
+
+    if(n >= array.length){
+      return array;
+    }
+
+    var newArray = [];
+    for (var i = array.length - n; i < array.length; i++){
+      newArray.push(array[i]);
+    }
+    return newArray;
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+
   };
 
   // Return all elements of an array that pass a truth test ('iterator' function argument)
